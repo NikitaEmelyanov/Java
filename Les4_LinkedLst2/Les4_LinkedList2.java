690bb2743a792d94a5b3b2b9ba8a6c18b7546fa6
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-02-27 00:16:10 +0300

    String1 file updated

--- Les4_LinkedLst2/Les4_LinkedList2.java
+++ Les4_LinkedLst2/Les4_LinkedList2.java
@@ -13,7 +13,22 @@ public class Les4_LinkedList2 {
         //называется хвост. Односвязный список представляет собой последовательность узлов
         //связанных ссылками, то есть у каждого узла есть ссылка на следующий узел. У
         //двусвязного есть ссылка и на следующий и на предыдущий узлы.
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(10);
 
+        System.out.println(myLinkedList);
+
+        System.out.println(myLinkedList.get(0));
+        System.out.println(myLinkedList.get(1));
+        System.out.println(myLinkedList.get(2));
+
+        myLinkedList.remove(1);
+        System.out.println(myLinkedList);
+
+        myLinkedList.remove(1);
+        System.out.println(myLinkedList);
 
 
 
