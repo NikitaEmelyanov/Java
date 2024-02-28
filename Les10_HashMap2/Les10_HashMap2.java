e0ad189b402bd03a75bdc314e07a1ea1dd723126
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-02-28 23:16:29 +0300

    String1 file updated

---
+++ Les10_HashMap2/Les10_HashMap2.java
@@ -0,0 +1,4 @@
+package Les10_HashMap2;
+
+public class Les10_HashMap2 {
+}
--- Les9_Contract_hashcode_equals/Les9_Contract_hashcode_equals.java
+++ Les9_Contract_hashcode_equals/Les9_Contract_hashcode_equals.java
@@ -8,24 +8,16 @@ public class Les9_Contract_hashcode_equals {
 
     Map<Person,String> map = new HashMap<>();
     Set<Person> set = new HashSet<>();
-    //Можно параметризовать объекты интерфейсов Set и Map любыми классами (В данном случае Person)
 
         Set<String> set1 = new HashSet<>();
 
         Person person1 = new Person(1, "Mike");
         Person person2 = new Person(1, "Mike");
-        //Если создать объекты используя такой же синтаксис и Map и Set будут отображать их как
-        // разные объекты, поскольку под каждый объект выделяется память
 
         set1.add("Hello");
         set1.add("Hello");
 
         System.out.println(set1);
-        //Но при таком синтаксисе отобразиться только один Hello, так как Java не пропустит дубликат,
-        //это происходит из-за скрытого метода equals(), который мы получаем от класса Object.
-        //Реализация equals() в Set и Map сравнивает объекты и ссылки на них и таким образом
-        // определяется дублика. Поскольку ссылка на оба объекта одинаковая, (поскольку не выделялась
-        //память под эти объекты) equals() может посчитать что это один и тот же объект
 
         map.put(person1,"123");
         map.put(person2,"123");
