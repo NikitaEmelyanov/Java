735f557ecf1d0052c5bc530990ff227dee7fab6b
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-02-28 22:49:35 +0300

    String1 file updated

---
+++ Les9_Contract_hashcode_equals/Person.java
@@ -0,0 +1,33 @@
+package Les9_Contract_hashcode_equals;
+
+import java.util.Objects;
+
+public class Person{
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+
+    }
+}
