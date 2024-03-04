45c6a9f0268f9e50dc62d8abb16365029f2d13a6
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-03-04 22:40:33 +0300

    String1 file updated

--- JavaPRO/Les14_Stack.java
+++ JavaPRO/Les14_Stack.java
@@ -1,4 +1,27 @@
 package JavaPRO;
 
+import java.util.Stack;
+
 public class Les14_Stack {
+    public static void main(String[] args) {
+        //Урок 14: Стек (Stack)
+        Stack<Integer> stack = new Stack<>();
+        //Класс Stack это противоположность от Queue тот кто зашел последним, выйдет первым
+        // (Last in / First out). Stack реализует класс Vector, который наследует все от интерфейса
+        //List, но с появлением LinkedList Vector устарел и больше не используется
+        stack.push(5); // Добавляет элемент в Stack
+        stack.push(5);
+        stack.push(1);
+
+        System.out.println(stack.peek()); //Просмотр последнего элемента из Stack
+
+        while (!stack.empty()) { // Возвращает true, если Stack пустой и возвращает false, если нет
+            System.out.println(stack.pop()); // Удаляет последний элемент из Stack
+        }
+
+
+
+
+
+    }
 }
---
+++ JavaPRO/Les15_Iterable.java
@@ -0,0 +1,7 @@
+package JavaPRO;
+
+public class Les15_Iterable {
+    public static void main(String[] args) {
+        //Урок 15: Интерфейс Iterable
+    }
+}
