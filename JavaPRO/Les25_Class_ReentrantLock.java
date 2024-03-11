f6f34c947ac17e8736b24b7f2695e6b525129ee1
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-03-11 15:13:32 +0300

    String1 file updated

---
+++ JavaPRO/Les25_Class_ReentrantLock.java
@@ -0,0 +1,56 @@
+package JavaPRO;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Les25_Class_ReentrantLock {
+    public static void main(String[] args) throws InterruptedException {
+        //Урок 25: Класс ReentrantLock
+        Task task = new Task();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                task.firstThread();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                task.secondThread();
+            }
+        });
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        task.showCounter();
+    }
+}
+class Task{
+    private int counter;
+    private Lock lock = new ReentrantLock();
+    private void increment(){
+        for (int i = 0; i < 10000; i++) {
+            counter++;
+        }
+    }
+    public void firstThread(){
+        lock.lock();
+        increment();
+        lock.unlock();
+        // Методы lock() и unlock() класса ReentrantLock() это аналог ключевого слова synchronized,
+        // так как только один поток может использовать lock() одновременно, а другой поток сможет
+        // использовать lock() только после вызова unlock() в предыдущем потоке
+    }
+    public void secondThread(){
+        lock.lock();
+        increment();
+        lock.unlock();
+    }
+    public void showCounter(){
+        System.out.println(counter);
+    }
+}
---
+++ JavaPRO/Les26_Class_Semaphore.java
@@ -0,0 +1,7 @@
+package JavaPRO;
+
+public class Les26_Class_Semaphore {
+    public static void main(String[] args) {
+        // Урок 26: Класс Семафор (Semaphore)
+    }
+}
