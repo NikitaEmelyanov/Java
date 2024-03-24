edb0b29be9c87d333569b1b6f1e142bfa111d36f
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-03-24 22:22:48 +0300

    String1 file updated

--- JavaPRO/Les33_Lambda_Part_1/Compartor_for_Lambda.java
+++ JavaPRO/Les33_Lambda_Part_1/Compartor_for_Lambda.java
@@ -1,6 +1,7 @@
 package JavaPRO.Les33_Lambda_Part_1;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Compartor_for_Lambda {
@@ -11,7 +12,27 @@ public class Compartor_for_Lambda {
         list.add("Goodbye");
         list.add("аb");
 
-        list.sort();
+        //Стандартный компартор
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() > o2.length()){
+                    return 1;
+                } else if (o1.length() < o2.length()) {
+                    return -1;
+                } else  {
+                    return 0;
+                }
+            }
+        });
+        System.out.println(list);
 
+        //Компартор, который реализоваван с помощью лямбды выражений
+        list.sort((s1,s2) -> {
+            if (s1.length()>s2.length()) return 1;
+            else if (s1.length()<s2.length()) return -1;
+            else return 0;
+        });
+        System.out.println(list);
     }
 }
