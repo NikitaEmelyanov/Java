f4079d551310f159707182f0e00179b2a1e26d28
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-03-24 22:12:09 +0300

    String1 file updated

--- JavaPRO/Les33_Lambda_Part_1.java
+++
@@ -1,8 +0,0 @@
-package JavaPRO;
-
-public class Les33_Lambda_Part_1 {
-    public static void main(String[] args) {
-        //Урок 33: Лямбда - выражения (часть I)
-
-    }
-}
---
+++ JavaPRO/Les33_Lambda_Part_1/Compartor_for_Lambda.java
@@ -0,0 +1,17 @@
+package JavaPRO.Les33_Lambda_Part_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Compartor_for_Lambda {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Hello");
+        list.add("Goodbye");
+        list.add("аb");
+
+        list.sort();
+
+    }
+}
