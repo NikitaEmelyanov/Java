4759c227b9ee4946a47ac23e8f6cc7f1ca19ff15
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-04-02 19:26:03 +0300

    String1 file updated

---
+++ Marathon/Day4/Task3.java
@@ -0,0 +1,41 @@
+package Marathon.Day4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        //3. Заполнить двумерный массив (матрицу) случайными числами от 0 до 50. Размер матрицы
+        // задать m=12,n=8 (m - размерность по строкам, n - размерность по колонкам). В консоль
+        // вывести индекс строки, сумма чисел в которой максимальна. Если таких строк несколько,
+        // вывести индекс последней из них.
+
+        //Пример сгенерированной матрицы(для простоты m=3, n=5):
+        //3 2 1 5 7 //сумма-18
+        // 1 2 5 6 2 //сумма-16
+        // 3 4 9 6 4 //сумма-26
+
+        int[][] matrice = new int[12][8];
+        Random random = new Random();
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[i].length; j++) {
+                matrice[i][j] = random.nextInt(50);
+            }
+        }
+        int maxSum = 0;
+        int maxSumIndex = 0;
+        for (int i = 0; i < matrice.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < matrice[i].length; j++) {
+                sum += matrice[i][j];
+            }
+
+            if (sum>=maxSum){
+                maxSum = sum;
+                maxSumIndex = i;
+            }
+        }
+        System.out.println("Индекс строки, где сумма чисел максимальна:" + maxSumIndex);
+    }
+}
