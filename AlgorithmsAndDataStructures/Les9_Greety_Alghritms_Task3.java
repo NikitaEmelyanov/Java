9cd45e93434f66e606d84f714c698663486ff724
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-04-09 19:11:53 +0300

    String1 file updated

--- AlgorithmsAndDataStructures/Les9_Greety_Alghritms_Task3.java
+++ AlgorithmsAndDataStructures/Les9_Greety_Alghritms_Task3.java
@@ -3,7 +3,7 @@ package AlgorithmsAndDataStructures;
 /**
  * Урок 9. Жадные алгоритмы. Задача о рюкзаке (Реализация)
  */
-public class Les8_Greety_Alghritms_Task3 {
+public class Les9_Greety_Alghritms_Task3 {
     public static void main(String[] args) {
 
     }
