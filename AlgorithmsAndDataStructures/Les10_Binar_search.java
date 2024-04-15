0ea64f35f946fccf74b622d371730eb1928c21a4
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-04-15 21:02:40 +0300

    String1 file updated

--- AlgorithmsAndDataStructures/Les10_Binar_search.java
+++ AlgorithmsAndDataStructures/Les10_Binar_search.java
@@ -5,7 +5,12 @@ package AlgorithmsAndDataStructures;
  */
 public class Les10_Binar_search {
     public static void main(String[] args) {
-        System.out.println(binarySearch( new int [] {-1,3,5,8,10,15,16,20},16));
+        System.out.println(binarySearch(new int[]{-1, 3, 5, 8, 10, 15, 16, 20}, 16));
+
+
+        int[] a = {-5, -2, 1, 5, 6, 8, 10, 20};
+        System.out.println(binarySearch2(a, 1, 0, a.length - 1));
+        // начальные low и high надо передавать в аргументы
 
 
         /**
@@ -21,13 +26,14 @@ public class Les10_Binar_search {
         //Нет - идем дальше
         //Сложность алгоритма в худшем случе O(n)
     }
+
     public static int linearSeach(int[] A, int k) {
         for (int i = 0; i < A.length; i++) {
             if (A[i] == k)
                 return i;
         }
-    return-1;
-}
+        return -1;
+    }
 
     /**
      * Двоичный поиск
@@ -77,18 +83,17 @@ public class Les10_Binar_search {
     //log2(50000) ~ 15.6
     //Пример из жизни - в словаре 50 000 слов нужное слово будет находиться не за 50 000 итераций в
     // худшем случае, а максимум за 16 итераций
-
-    public static int binarySearch(int [] a, int  key) {
+    public static int binarySearch(int[] a, int key) {
         int low = 0;
         int high = a.length - 1;
 
         while (low <= high) {
             int middle = low + (high - low) / 2;
 
-            if (key < a[middle]){
-                high = middle -1;
-            }else if (key > a[middle]) {
-                low = middle +1;
+            if (key < a[middle]) {
+                high = middle - 1;
+            } else if (key > a[middle]) {
+                low = middle + 1;
             } else {
                 return middle;
             }
@@ -105,7 +110,28 @@ public class Les10_Binar_search {
     // выходим из рекурсии, либо подмассив пустой, искомого числа в массиве нет и мы возвращаем -1.
     // 3) Рекурсивный вызов либо левого подмассива, либо правого подмассива.
 
-    //Сигнатура метода (один из возможных вариантов):
-    //public int binarySearch (int[] sortedArray, int key, int low, int high)
-}
 
+    //Сигнатура метода (один из возможных вариантов):
+    public static int binarySearch2(int[] sortedArray, int key, int low, int high) {
+        // считаем индекс центрального элемента
+        int middle = low + (high - low) / 2;
+
+        // base case (условие выхода) - регион поиска пустой
+        if (low > high) {
+            // не нашли элемента, который равен ключу
+            return -1;
+        }
+
+        if (key == sortedArray[middle]) {
+            // в случае, если элемент в центре равняется ключу (нашли элемент)
+            return middle;
+        } else if (key < sortedArray[middle]) {
+            // рекурсивный вызов для левого подмассива
+            // не забывайте здесь ключевое слово return (подумайте, зачем оно нужно)
+            return binarySearch2(sortedArray, key, low, middle - 1);
+        } else {
+            // рекурсивный вызов для правого помассива
+            return binarySearch2(sortedArray, key, middle + 1, high);
+        }
+    }
+}
---
+++ Java_EE/Les3_FirstJavaEEProject_FirstServlet.java
@@ -0,0 +1,7 @@
+package Java_EE;
+
+/**
+ * Урок 3. Практика. Создание Java EE приложения. Первый сервлет
+ */
+public class Les3_FirstJavaEEProject_FirstServlet {
+}
