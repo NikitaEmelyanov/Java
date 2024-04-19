1a578c2d69eaa897baf92ea12e0aac51e8c63e14
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-04-19 15:09:40 +0300

    String1 file updated

--- Marathon/Day12/Task5/MusicBand.java
+++ Marathon/Day12/Task5/MusicBand.java
@@ -1,13 +1,13 @@
-package Marathon.Day12.Task4;
+package Marathon.Day12.Task5;
 
 import java.util.List;
 
 public class MusicBand {
     private String name ;
     private int year;
-    private List<String> members;
+    private List<MusicArtist> members;
 
-    public MusicBand(String name, int year, List<String> members) {
+    public MusicBand(String name, int year, List<MusicArtist> members) {
         this.name = name;
         this.year = year;
         this.members = members;
@@ -29,15 +29,16 @@ public class MusicBand {
         this.year = year;
     }
 
-    public List<String> getMembers() {
+    public List<MusicArtist> getMembers() {
         return members;
     }
 
-    public void setMembers(List<String> members) {
+    public void setMembers(List<MusicArtist> members) {
         this.members = members;
     }
+
     public static void transferMembers (MusicBand a, MusicBand b){ // Статический метод
-        for (String member: a.getMembers())
+        for (MusicArtist member: a.getMembers())
             b.getMembers().add(member);
 
         a.getMembers().clear();
--- Marathon/Day12/Task5/Task5.java
+++ Marathon/Day12/Task5/Task5.java
@@ -1,5 +1,8 @@
 package Marathon.Day12.Task5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 5. *Выполнять в подпапке task5 в day12*
  * Скопировать MusicBand из прошлого задания и доработать - теперь у участника музыкальной группы
@@ -12,6 +15,30 @@ package Marathon.Day12.Task5;
  */
 public class Task5 {
     public static void main(String[] args) {
+        List<MusicArtist> members1 = new ArrayList<>();
+        members1.add(new MusicArtist("Grace Slick",50));
+        members1.add(new MusicArtist("Marty Balin",35));
+        members1.add(new MusicArtist("Paul Kantnerk",24));
+        members1.add(new MusicArtist("Jorma Kaukonen",40));
+        members1.add(new MusicArtist("Jack Casady",45));
+        members1.add(new MusicArtist("Spencer Dryden",42));
 
+        Marathon.Day12.Task5.MusicBand band1 = new Marathon.Day12.Task5.MusicBand("Jafferson Airplane",1965, members1);
+
+        List<MusicArtist> members2 = new ArrayList<>();
+        members2.add(new MusicArtist("John Lennon",32));
+        members2.add(new MusicArtist("Paul McCartney",54));
+        members2.add(new MusicArtist("Ringo Starr",61));
+        members2.add(new MusicArtist("George Harrison",45));
+
+        Marathon.Day12.Task5.MusicBand band2 = new Marathon.Day12.Task5.MusicBand("The Beatles",1960,members2);
+
+        band1.printMembers();
+        band2.printMembers();
+
+        Marathon.Day12.Task5.MusicBand.transferMembers(band1,band2);
+
+        band1.printMembers();
+        band2.printMembers();
     }
 }
